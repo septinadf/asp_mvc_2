3e784da99686eda4c95dfd4cd0c4a50728351c34
User <septinarika90@gmail.com>
2017-01-31 13:24:45 +0700

    finish

---
+++ asp_mvc_3/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using asp_mvc_3.Models.ViewModel;
+using asp_mvc_3.Models.EntityManager;
+
+namespace asp_mvc_3.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult SignUp() {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(UserSignUpView USV) { if (ModelState.IsValid) {
+                UserManager UM = new UserManager(); if (!UM.IsLoginNameExist(USV.LoginName)) { UM.AddUserAccount(USV); FormsAuthentication.SetAuthCookie(USV.FirstName, false);
+                    return RedirectToAction("Welcome", "Home"); } else ModelState.AddModelError("", "Login Name already taken."); } return View(); }
+    }
+}
